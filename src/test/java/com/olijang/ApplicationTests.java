fe0044ac03dc0409a8676aa84bf37f5a865b3f33
lsuns <j76067469@gmail.com>
2023-10-31 19:32:34 +0900

    first Commit

---
+++ src/test/java/com/olijang/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olijang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
