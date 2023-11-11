70c603b00115f5994ab8086b8ba9769f0e10c5bf
ksostuft <ksostuft@gmail.com>
2023-11-11 13:28:13 +0900

    20231111-product-004 Service 변경 w.product.productCode = p.productCode

--- src/main/java/com/mergeco/oiljang/product/service/ProductService.java
+++ src/main/java/com/mergeco/oiljang/product/service/ProductService.java
@@ -287,7 +287,7 @@ public class ProductService {
     }
 
     public List<SellingListDTO> selectSellingList(int offset, int limit, int refUserCode) {
-        String jpql = "SELECT new com.mergeco.oiljang.product.dto.SellingListDTO(p.productCode, p.productThumbAddr, p.productName, p.productPrice,(SELECT Count(w.wishCode) FROM WishList w WHERE w.product.productCode = :productCode), p.SellStatus.sellStatus)" +
+        String jpql = "SELECT new com.mergeco.oiljang.product.dto.SellingListDTO(p.productCode, p.productThumbAddr, p.productName, p.productPrice,(SELECT Count(w.wishCode) FROM WishList w WHERE w.product.productCode = p.productCode), p.SellStatus.sellStatus)" +
                 " FROM WishList w JOIN w.product p WHERE w.refUserCode = :refUserCode ORDER BY w.wishCode DESC";
         List<SellingListDTO> wishList = entityManager.createQuery(jpql)
                 .setParameter("refUserCode", refUserCode)
