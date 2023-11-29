e186cf866f218f6f40ce2ba52a22420e7141ca65
ksostuft <ksostuft@gmail.com>
2023-11-29 19:12:22 +0900

    20231129-product-004 날짜 형식을 yyyy-mm-dd로 맞춰주었습니다.

--- src/main/java/com/mergeco/oiljang/myCalendar/dto/MyCalendarDTO.java
+++ src/main/java/com/mergeco/oiljang/myCalendar/dto/MyCalendarDTO.java
@@ -1,6 +1,8 @@
 package com.mergeco.oiljang.myCalendar.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -13,5 +15,8 @@ public class MyCalendarDTO {
     private int myCalendarCode;
     private int refUserCode;
     private String calendarContent;
+
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate calendarDate;
 }
