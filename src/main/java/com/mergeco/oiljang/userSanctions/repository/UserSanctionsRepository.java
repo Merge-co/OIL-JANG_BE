c9535cad069c24f12a9051b5eeb9eae87c922851
lsuns <j76067469@gmail.com>
2023-12-06 15:45:41 +0900

    :fire: [BE][FEATURE]사용자제제 update완료

--- src/main/java/com/mergeco/oiljang/userSanctions/repository/UserSanctionsRepository.java
+++ src/main/java/com/mergeco/oiljang/userSanctions/repository/UserSanctionsRepository.java
@@ -1,9 +1,19 @@
 package com.mergeco.oiljang.userSanctions.repository;
 
 import com.mergeco.oiljang.userSanctions.entity.UserSanctions;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserSanctionsRepository extends JpaRepository<UserSanctions, Integer> {
+
+    @Query(value =
+            "SELECT s.ref_user_code, s.sanctions_date, s.manager_date, s.sanctions_code " +
+                    "FROM user_sanctions s " +
+                    "WHERE s.ref_user_code = :refUserCode ", nativeQuery = true)
+    UserSanctions findByRefUserCode(@Param("refUserCode") int refUserCode);
 }
